badc226332fddf812abf547364d4157339592722
adamlky02 <adamliewky@gmail.com>
2025-10-27 23:04:27 +0800

    to6

---
+++ js/load-data.js
@@ -0,0 +1,20 @@
+d3.csv("data/Ex6_TVdata.csv", d => ({
+    brand: d.brand,
+    model: d.model,
+    screenSize: +d.screenSize,
+    screenTech: d.screenTech,
+    energyConsumption: +d.energyConsumption,
+    star: +d.star
+})).then(data => {
+    console.log(data);
+
+    drawHistogram(data);
+    drawScatterplot(data);
+    populateFilters(data);
+
+    createTooltip();
+    handleMouseEvents();
+
+}).catch(error => {
+    console.error("Error loading the data: ", error);
+})
